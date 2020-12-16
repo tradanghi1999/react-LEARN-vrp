e968fb09fb260f6ada828e42cc54c5a8601a029d
tradanghi1999 <tradanghi1999@gmail.com>
2020-12-16 12:42:45 +0700

    1 file modified

--- src/lib/api.js
+++ src/lib/api.js
@@ -119,16 +119,16 @@ const API = {
 
         let rRG = rG.map((x, i) => {
           return {
-            capacity_percentage: (
-              pointOnRoutes[i]
-                .filter(x => x.subtype == "customer")
-                .map(x => x.data.weight)
-                .reduce((a, b) => a + b) / vehicles.weight_limit
-            ).toFixed(2),
+            capacity_percentage: pointOnRoutes[i]
+              .filter(x => x.subtype == "customer")
+              .map(x => x.data.weight)
+              .reduce((a, b) => a + b)
+              // vehicles.weight_limit
+              .toFixed(2),
             routeG: x
           };
         });
-
+        console.log(pointOnRoutes);
         return rRG;
       })
     );
