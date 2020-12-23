0f960a2277215915b7b8af82f413327c45fd77f0
tradanghi1999 <tradanghi1999@gmail.com>
2020-12-23 10:51:12 +0700

    1 file modified

--- src/component/cordinating/routetable/routerow/route/routechild/late_start.js
+++ src/component/cordinating/routetable/routerow/route/routechild/late_start.js
@@ -4,7 +4,9 @@ const LateStart = props => {
   const { style, data } = props;
   let width =
     (data.time_value <= 0 ? 0.01 : data.time_value) * style.widthRatio;
-  return <div className="late_start" style={{ width: width + "px" }} />;
+  let minWidth = (data.time_value_min) * style.widthRatio;
+  let maxWidth = (data.time_value_max) * style.widthRatio;
+  return <div className="late_start" style={{ minWidth:  }} />;
 };
 
 LateStart.defaultProps = {
